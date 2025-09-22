fa8ce4feec7da8350cec03711f395ba0e5eb3c2d
Minsung <pauaup@naver.com>
2025-09-22 12:05:53 +0900

    use frame instead of rgb_frame for cv2 draws

--- src/yolo_camera.py
+++ src/yolo_camera.py
@@ -27,18 +27,18 @@ while True:
         conf = obj[4]
 
         # TODO: 인식된 정확도(confidence)와 클래스를 label로 구성
-        cv2.putText(rgb_frame, f'{model.names[cls]} ({conf:.2f}) at [{x1}, {y1}, {x2}, {y2}]',
+        cv2.putText(frame, f'{model.names[cls]} ({conf:.2f}) at [{x1}, {y1}, {x2}, {y2}]',
                     (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 3)
-        cv2.putText(rgb_frame, f'{model.names[cls]} ({conf:.2f}) at [{x1}, {y1}, {x2}, {y2}]',
+        cv2.putText(frame, f'{model.names[cls]} ({conf:.2f}) at [{x1}, {y1}, {x2}, {y2}]',
                     (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1)
 
         # TODO: OpenCV를 이용해서 해당 좌표에 사각형과 text를 출력
-        cv2.rectangle(rgb_frame, (x1, y1), (x2, y2), (255, 255, 255), 1)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 255, 255), 1)
         obj_info = list(map(int, obj))
         print(f"Object {i}: {model.names[obj_info[5]]}")
 
     # TODO: 화면 표시
-    cv2.imshow('view', cv2.cvtColor(rgb_frame, cv2.COLOR_RGB2BGR))
+    cv2.imshow('view', frame)
 
     # TODO: 종료를 위한 key 처리
     if cv2.waitKey(1) & 0xFF == 27:
